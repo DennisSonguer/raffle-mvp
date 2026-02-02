79c7b9ccc9d698590df3dd6a3805941a54e3970f
Dennis <dennis.songuer007@gmail.com>
2026-02-02 17:00:56 +0100

    remove wrong lockfile

--- src/app/raffles/page.tsx
+++ src/app/raffles/page.tsx
@@ -1,7 +1,13 @@
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
+import { Suspense } from "react";
 import RafflesClient from "./RafflesClient";
 
 export default function Page() {
-  return <RafflesClient />;
+  return (
+    <Suspense fallback={null}>
+      <RafflesClient />
+    </Suspense>
+  );
 }
